f538d19a16595b342d0484ae6ce5df060eb0b792
Thomasdalsae <72367829+Thomasdalsae@users.noreply.github.com>
2021-11-01 00:15:30 +0100

    reee

--- thomas_dalstein_Compulsory3_connectFour/main.cpp
+++ thomas_dalstein_Compulsory3_connectFour/main.cpp
@@ -3,6 +3,7 @@
 #include <conio.h>
 #include <Windows.h>
 
+
 struct tiles
 {
 
@@ -12,13 +13,15 @@ struct tiles
 bool animDrop(int, std::vector<std::vector<tiles>>&);
 void boardLayout(std::vector<std::vector<tiles>>);
 bool dropSlot(int, std::vector<std::vector<tiles>>);
+bool winChecker(std::vector<std::vector<tiles>>& animboardChecker);
 
-
-
+int x{};
+int y{};
 int witdh = 7;
-int height = 8;
+int height = 6;
 int turn{};
-
+char playerSymbol = 'x';
+int inaRow{};
 int main(){
 	
 	
@@ -37,64 +40,45 @@ int main(){
 }
 
 
-bool dropSlot(int boardSize, std::vector<std::vector<tiles>> Aboard) {
+bool dropSlot(int boardSize, std::vector<std::vector<tiles>>aboard) {
 
-	int slot{};
+	
 	
 		while (true)
 		{
 			
-			
-			
-			
-			for (size_t i = 0; i < boardSize; i++)
-			{
-
-				if (slot == i)
-				{
-					std::cout << "(" << 'x' << ")";
-
-				}
-				else
-				{
-					std::cout << "( )";
-				}
-
-			}
-			std::cout << std::endl;
-			
-
-				boardLayout(Aboard);
+				boardLayout(aboard);
 
 
-			char b = _getch();
+ 			char b = _getch();
 
 				switch (b)
 				{
 					//Player goes left
 				case 'a': case 'A':
 
-					slot += -1;
+					x += -1;
 
-					if (slot < 0)
+					if (x < 0)
 					{
-						slot = boardSize - 1;
+						x = boardSize - 1;
 					}
 
 					break;
 					//Player goes right
 				case 'd': case 'D':
-					slot += 1;
+					x += 1;
 
-					if (slot == boardSize)
+					if (x == boardSize)
 					{
-						slot = 0;
+						x = 0;
 
 					}
 					break;
 
 				case ' ':
-					animDrop(slot, Aboard);
+					
+					animDrop(x, aboard);
 					turn++;
 
 					break;
@@ -103,13 +87,107 @@ bool dropSlot(int boardSize, std::vector<std::vector<tiles>> Aboard) {
 					break;
 				}
 
-				system("cls");
+				
 			
 			}
+		
+}
+
+bool winChecker(std::vector<std::vector<tiles>> &animboardChecker)
+{
+	inaRow = 0;
+	int row = x;
+	int column = y;
+	
+	while (true)
+	{
+		if (row < 0 || row >= animboardChecker.size() || !playerSymbol) {
+			// handle edge in y-direction
+			break;
+		}
+		if (column < 0 || column >= animboardChecker[0].size() || !playerSymbol) {
+			// handle edge in x-direction
+
+			break;
+		}
+			if (animboardChecker[row][column].tileSymbol == animboardChecker[row][column + 1].tileSymbol && animboardChecker[row][column].tileSymbol == animboardChecker[row][column + 2].tileSymbol)
+			{
+				return true;
+									
+			}
+			
+
+		
+
+		
+			return false;
+
+
+	}
+
+	
 	
 }
-void boardLayout(std::vector<std::vector<tiles>> board) {
 
+void boardLayout(std::vector<std::vector<tiles>> board) {
+	system("cls");
+
+	if (turn % 2 == 0)
+	{
+		playerSymbol = 'x';
+	}
+	else
+	{
+		playerSymbol = 'o';
+	}
+
+	for (size_t i = 0; i < board[0].size(); i++)
+	{
+
+		if (x == i)
+		{
+			std::cout << " " << '|' << " ";
+
+		}
+		else
+		{
+			std::cout << "   ";
+		}
+
+	}
+	std::cout << std::endl;
+	for (size_t i = 0; i < board[0].size(); i++)
+	{
+
+		if (x == i)
+		{
+			std::cout << " " << 'v' << " ";
+
+		}
+		else
+		{
+			std::cout << "   ";
+		}
+
+	}
+	std::cout << std::endl;
+	for (size_t i = 0; i < board[0].size(); i++)
+	{
+
+		if (x == i)
+		{
+			std::cout << "(" << playerSymbol << ")";
+
+		}
+		else
+		{
+			std::cout << "( )";
+		}
+
+	}
+	std::cout << std::endl;
+	
+	
 	
   	for (int column = board.size() - 1; column >= 0; column--)
 		
@@ -131,59 +209,56 @@ void boardLayout(std::vector<std::vector<tiles>> board) {
 	
 	
 	std::cout << turn;
+	
 }
 
 bool animDrop(int slotdrop,std::vector<std::vector<tiles>>& animboard) {
-
-	
 	
 	
 
+
+	
 	for (int i = animboard.size() - 1; i >= 0; i--)
 	{
-		if (animboard[i][slotdrop].tileSymbol == 'x')
+		if (animboard[i][slotdrop].tileSymbol == 'x' || animboard[i][slotdrop].tileSymbol == 'o')
 		{
 			i++;
-			animboard[i][slotdrop].tileSymbol = 'x';
+			animboard[i][slotdrop].tileSymbol = playerSymbol;
 
 			break;
 		}
 		if (animboard[i][slotdrop].tileSymbol == '*' )
 		{
 			
-			animboard[i][slotdrop].tileSymbol = 'x';
+			animboard[i][slotdrop].tileSymbol = playerSymbol;
 			
 			
 		}
-		if (i >= 1) {
-			animboard[i][slotdrop].tileSymbol = 'x';
+		if (i >= 0) {
+			animboard[i][slotdrop].tileSymbol = playerSymbol;
+			
+			
+			y = i;
+			x = slotdrop;
 			boardLayout(animboard);
-			Sleep(200);
+			
+			
+			Sleep(100);
+			if (i == 0)
+			{
+				break;
+			
+			}
 			animboard[i][slotdrop].tileSymbol = '*';
-			system("cls");
 		}
 		
-		
-	
 	}
-	
+	winChecker(animboard);
+	if (true) {
+		std::cout << "three in a row ";
+	}
 
+	Sleep(500);
 	
 	return 0;
 	}
-	
-
-	
-
-	
-	
-
-
-	
-//for (size_t i = 0; i < animboard[0].size(); i++)
-//{
-//	if (animboard[slotdrop][i].tileSymbol == '*')
-//		animboard[slotdrop][i].tileSymbol = animboard[0][1].playerSymbol;
-//
-//}
-//return false;
